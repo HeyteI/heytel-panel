db9923754617ad8fd9d529c2f130a698fb5bcca6
style77 <stylek777@gmail.com>
2022-09-12 00:11:00 +0200

    fixed circles in background

--- src/components/login/circles.js
+++ src/components/login/circles.js
@@ -3,7 +3,7 @@ import React from "react"
 export function Circles() {
     return (
         <div className="Circles">
-            <div className="footer-texture absolute bottom-0 left-0 h-32 w-full" style={{ backgroundImage: "url('./Group_106.svg')", zIndex: -1}}></div>
+            <div className="footer-texture fixed bottom-0 left-0 h-32 w-full" style={{ backgroundImage: "url('./Group_106.svg')", zIndex: -1}}></div>
         </div>
     )
 }
